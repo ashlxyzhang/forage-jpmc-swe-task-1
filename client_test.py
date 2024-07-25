fdffcdfb3ff4ca01ec2eb35a7daee879f7ccb9be
Ashley <ashleyoftexas@gmail.com>
2024-07-25 18:15:23 -0500

    patched trading errors and made unit tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,29 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price']) / 2))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self):
+    prices = [{'price_a': 100, 'price_b': 50}, {'price_a': 50, 'price_b': 100}, 
+              {'price_a': 50, 'price_b': 50}, {'price_a': 1, 'price_b': 1}]
+    
+    for price in prices:
+      self.assertEqual(getRatio(price['price_a'], price['price_b']), price['price_a'] / price['price_b'])
+
+  def test_getRatio_calculateRatioPriceBZero(self):
+    prices = [{'price_a': 100, 'price_b': 0}, {'price_a': 50, 'price_b': 0}]
+    
+    for price in prices:
+      self.assertEqual(getRatio(price['price_a'], price['price_b']), None)
+
+  def test_getRatio_calculateRatioPriceAZero(self):
+    prices = [{'price_a': 0, 'price_b': 100}, {'price_a': 0, 'price_b': 50}]
+    
+    for price in prices:
+      self.assertEqual(getRatio(price['price_a'], price['price_b']), 0)
 
 
 
